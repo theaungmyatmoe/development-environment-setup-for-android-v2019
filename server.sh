5dbfb3e8bdef3777aee6448cfde36a5f2fbf32ba
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-09 07:45:05 +0630

    Added Server Service File

---
+++ server.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+if [[ "$#" -eq 1 ]]; then
+case $1 in
+ 'start')
+  apachectl start
+  #mysqld_safe
+  sh $PREFIX/etc/init.d/mysql start
+  clear
+  echo -e "Apache server is running at port \e[32mlocalhost:8080\e[0m.\nMariadb is running at port \e[32m3306\e[0m."
+  ;;
+  'stop')
+  apachectl stop
+  kill $(ps aux | grep '[m]ysql' | awk '{print $2}')
+  echo -e "\e[32mApache and Mariadb stopped.\e[0m"
+  ;;
+  *)
+  echo -e "You forgot \e[32mstart|stop option!\e[0m"
+  ;;
+esac
+else
+  echo -e "You forgot \e[32mstart|stop option!\e[0m"
+fi
+
